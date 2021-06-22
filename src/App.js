2911df75d85c6df3a00858722e9913b71c20d8af
Kolawole Olulana <kolawolegolulana@gmail.com>
2021-06-22 14:17:46 +0200

    second change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Guinea Web Dev Week</h1>
+        <h1>Hello from Guinea Web Dev Week - 2 </h1>
       </header>
     </div>
   );
