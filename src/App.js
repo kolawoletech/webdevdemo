ea7dc999435ed459313a4b1e8031d4c6d37d71ab
Kolawole Olulana <kolawolegolulana@gmail.com>
2021-06-22 14:55:27 +0200

    ssd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Guinea Web Dev Week - 2 </h1>
-      </header>
-    </div>
-  );
-}
+    import React from 'react';
+    import logo from './logo.svg';
+    import './App.css';
+    import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-export default App;
+    function App() {
+    return (
+        <div className="App">
+        <header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>We now have Auth!</h1>
+        </header>
+        <AmplifySignOut />
+        </div>
+    );
+    }
+
+    export default withAuthenticator(App);
